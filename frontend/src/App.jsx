9e2b03b8fa6fd1ecc73aad6f54de8f47fe1099f1
Muhammad Taleb <glooty19@gmail.con>
2026-01-10 22:45:40 +0300

    adding layout for quick selection of game play mode

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ChessBoardExample from './components/ChessBoardExample';
 import './App.css';
+import ChessGrid from './components/ChessGrid';
 
 function App() {
   return (
     <div className="App">
       <ChessBoardExample />
+      <ChessGrid/>
     </div>
   );
 }
